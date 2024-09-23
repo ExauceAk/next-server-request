36bb1f8ad8ae9294ab7f8b4a326f597169830011
ExauceAk <exauceakplaka@gmail.com>
2024-09-23 09:26:04 +0100

    sercice class

---
+++ services/services.ts
@@ -0,0 +1,56 @@
+// import { createMutation } from 'react-query-kit';
+import { useQuery } from '@tanstack/react-query';
+import { FetchService } from './api-sdk/lib/utils/fetch';
+import { ApiError } from './api-sdk/errors';
+
+export class Service {
+  private port: number;
+
+  constructor(port: number) {
+    this.port = port;
+  }
+
+
+  // fetchService: FetchService = new FetchService({
+  //   requestInterceptor: async (config) => ({
+  //     ...config,
+  //     headers: {
+  //       ...config.headers,
+  //       Authorization: `Bearer ${await getToken()}`,
+  //     },
+  //   }),
+  // });
+
+   fetchService: FetchService = new FetchService();
+
+  // cettte methode fonctionne
+
+  // useGet = () =>
+  //   useQuery({
+  //     queryKey: ['/type'],
+  //     queryFn: () => api.get(`/type-office`),
+  //   });
+
+    useGet = (queryKey: [string]) => {
+       return  useQuery({
+        queryKey: queryKey,
+        queryFn: () => this.getAllTypeOffice(),
+    });
+  };
+
+
+    getAllTypeOffice = async () => {
+      const response = await this.fetchService.get("/type-office");
+    
+      if (response.ok) {
+        return await response.json();
+      }
+    
+      throw new ApiError("Failed to fetch events");
+    };
+
+  // useCreate = createMutation({
+  //   mutationKey: ['/register'],
+  //   mutationFn: (data: any) => api.post('/register', data),
+  // });
+}
